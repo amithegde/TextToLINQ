db07d3c588d5c23dc89c10acc5888194bd7d8ca0
Amit <amits.hegde@gmail.com>
2014-03-06 02:31:08 -0800

    bin and obj folders purged

--- TextToLINQ/Program.cs
+++ TextToLINQ/Program.cs
@@ -17,6 +17,7 @@ namespace TextToLINQ
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+			
         }
     }
 }
